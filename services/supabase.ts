36d42f54250d0287e1eb4f3000c63b0607de6c99
Pranav-AJ <pranavaj3721@gmail.com>
2025-11-29 14:04:25 +0530

    second commit

--- services/supabase.ts
+++ services/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // You will get these keys from your Supabase Project Dashboard
 const supabaseUrl = 'https://ndzojoilmmsucodpmdim.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kem9qb2lsbW1zdWNvZHBtZGltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNDkwNzQsImV4cCI6MjA3OTkyNTA3NH0.ekIGM8M2YRJ0iKTIWOtu5xiLrdptQy53jbajc_9Knfg';
+const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_API_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
